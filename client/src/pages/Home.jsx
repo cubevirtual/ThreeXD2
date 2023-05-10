897e2b35c6a8bd44126f7787645ce77e36e093e5
Magic <magic@Magics-MacBook-Pro.local>
2023-05-09 23:26:21 -0700

    first commit3

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,56 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+import { CustomButton } from '../components';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion';
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation('left')}>
+          <motion.header {...slideAnimation("down")}>
+            <img 
+              src='./rassena.png'
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                MAGIC <br className="xl:block hidden" /> REALITY
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+              Создайте свою уникальную и эксклюзивную футболку с помощью нашего совершенно нового инструмента 3D-настройки. Дайте волю своему воображению и определите свой собственный стиль. <strong></strong>{" "}
+              </p>
+
+              <CustomButton 
+                type="filled"
+                title="Попробуй"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default Home
